1e857989b231515a1aad1e06c73383092ad176c6
nurseiit <nurseiiit.tursunkulov@2peaches.de>
2024-12-28 11:51:28 +0100

    Add package sources

--- Package.swift
+++ Package.swift
@@ -4,5 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyPackage"
+    name: "MyPackage",
+    platforms: [.iOS(.v15), .macOS(.v10_15)],
+    // 4
+    products: [
+        .library(
+            name: "MyPackage",
+            targets: ["MyPackage"]),
+    ],
+    // 5
+    dependencies: []
 )
